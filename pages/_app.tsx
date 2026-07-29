28c0a8d075649f530acdfd5f15e613a18c72368e
Chris Sealey <csealey1@gmail.com>
2026-07-29 19:15:48 +0100

    Establish editorial visual foundation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,11 @@
 import type { AppProps } from "next/app";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/newsreader/500.css";
+import "@fontsource/newsreader/600.css";
+import "@fontsource/newsreader/700.css";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
